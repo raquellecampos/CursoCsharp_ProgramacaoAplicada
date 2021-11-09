fa16c5d0642eddbd843d2ecf47460848d4e513ff
raquellecampos <raquellecampos12@gmail.com>
2021-11-09 08:07:26 -0400

    aulas 123

---
+++ CursoCsharp_ProgramacaoAplicada/Variaveis.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CursoCsharp_ProgramacaoAplicada
+{
+    class Variaveis
+    {
+        static void Main(string[] args)
+        {
+            //string nome = "Raquelle";
+
+            //char letra = 'R';
+            //bool chovendo = true;
+            //int idade = 35;
+            //float numeroQuebrado = 10.5f;
+            //double numeroQuebradoDouble = 10.5;
+            //decimal numeroQuebradoDecimal = 10.5m;
+            //sbyte idade2;
+
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite seu sobrenome: ");
+            string sobreNome = Console.ReadLine();
+
+            Console.Write("Digite um número: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite outro número: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Meu nome é {nome} {sobreNome}");
+
+
+            ///conversao////
+            float numero = 10.6546735f;
+            int numero2 = (int)numero; //vai ter perda de informação
+
+            //Console.WriteLine(numero2);
+            //Console.WriteLine(n1*n2);
+            //Console.ReadKey();
+
+
+            /////Array////
+           int[] numeros = new int[] { 1,2,3};
+            string[] nomes = new string[]
+            {
+                "Raquelle",
+                 "Isaac",
+
+            };
+            
+        }
+    }
+}
---
+++ LogicaDeProgramacao/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LogicaDeProgramacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 7;
+
+            if(numero< 5)   ///encadeamento
+            {
+                Console.WriteLine("O número é menor do que 5");
+            }
+            else if(numero < 8)
+            {
+                Console.WriteLine("O número é menor do que 8");
+            }
+            else
+            {
+                Console.WriteLine("O número é menor do que 5 e 8");
+            }
+
+
+            int num = 10;
+
+            while(num < 100)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+
+           
+        }
+    }
+}
