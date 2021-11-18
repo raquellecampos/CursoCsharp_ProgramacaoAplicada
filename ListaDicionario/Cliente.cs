7466ad03f19f1fcbcd31f2ff2b676b9a53902db9
raquellecampos <raquellecampos12@gmail.com>
2021-11-18 08:24:36 -0400

    aula

---
+++ ListaDicionario/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDicionario
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public char Sexo { get; set; }
+        public double Limite { get; set; }
+
+        public Cliente(string nome, int idade, char sexo, double limite)
+        {
+            Nome = nome;
+            Idade = idade;
+            Sexo = sexo;
+            Limite = limite;
+        }
+    }
+}
---
+++ ListaDicionario/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListaDicionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Cliente> clientes = new List<Cliente>();
+            clientes.Add(new Cliente("Raquelle", 35, 'F', 500));
+            clientes.Add(new Cliente("Aloisio", 48, 'M', 700));
+            clientes.Add(new Cliente("Isaac", 20, 'M', 800));
+
+            foreach(Cliente cliente in clientes)
+            {
+                Console.WriteLine(cliente.Nome + " - " + "R$" + cliente.Limite);
+            }
+
+
+
+
+            //List<string> nomes = new List<string>();
+            //nomes.Add("Raquelle");
+            //nomes.Add("Isaac Henrique");
+            //nomes.Add("Aloisio");
+            //nomes.Add("Anna Luísa");
+
+            //nomes.Remove("Raquelle");
+
+            //Console.WriteLine(nomes[1] + " - " + nomes.Count); //Count retorna a quantidades de itens dentro de nome
+
+            ////foreach(string item in nomes)
+            ////{
+            ////    Console.WriteLine(item);
+            ////}
+
+            //Dictionary<int, string> dicionario = new Dictionary<int, string>(); 
+
+            //    dicionario.Add(1, "camiseta");
+            //    dicionario.Add(2, "calça");
+            //    dicionario.Add(3, "sapato");
+
+            //foreach(KeyValuePair<int, string> item in dicionario) //chave e valor
+            //{
+            //    Console.WriteLine(item.Key + " - " + item.Value); 
+            //}
+        }
+    }
+}
